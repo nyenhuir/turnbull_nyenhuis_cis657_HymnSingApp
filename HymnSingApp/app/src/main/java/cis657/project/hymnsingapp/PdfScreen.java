a0b72fbf3598155bd1c89dc360919eae39400533
Jemima Turnbull <jemimaturnbull@Jemimas-MacBook-Air.local>
2019-06-10 19:56:10 -0400

    fixes #1

---
+++ HymnSingApp/app/src/main/java/cis657/project/hymnsingapp/PdfScreen.java
@@ -0,0 +1,51 @@
+package cis657.project.hymnsingapp;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class PdfScreen extends AppCompatActivity {
+
+    String url = "https://hymnary.org";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pdf_screen);
+        String s = getIntent().getStringExtra("n1");
+        url = url + s;
+        WebView wv = (WebView) findViewById(R.id.the_webview);
+
+        wv.setWebViewClient(new WebViewClient() {
+            public void onReceivedError(WebView view, int errorCode, String description, String   failingUrl) {
+
+            }
+        });
+        wv.loadUrl("https://docs.google.com/gview?embedded=true&url=" + url);
+
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
